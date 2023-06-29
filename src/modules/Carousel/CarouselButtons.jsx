3c866686661b6bea6b1a44d217a4528724847579
FrancoSMiño <frankow222@gmail.com>
2023-06-29 11:07:41 -0300

    1.0

--- src/modules/Carousel/CarouselButtons.jsx
+++ src/modules/Carousel/CarouselButtons.jsx
@@ -1,14 +1,40 @@
-export default function CarouselButtons({onClick}){
-    return(
+export default function CarouselButtons({ onClick, current }) {
+  return (
     <div className="carousel__buttons-container">
-        <button className='carousel__button--next' onClick={onClick} value={'next'}>&gt;</button>
-        <div className='carousel__index-buttons'>
-            <button className='carousel__index-buttons__button' onClick={onClick} value={0}></button>
-            <button className='carousel__index-buttons__button' onClick={onClick} value={1}></button>
-            <button className='carousel__index-buttons__button' onClick={onClick} value={2}></button>
-            <button className='carousel__index-buttons__button' onClick={onClick} value={3}></button>
-            <button className='carousel__index-buttons__button' onClick={onClick} value={4}></button>
-        </div>
+      <button
+        className="carousel__button--next"
+        onClick={onClick}
+        value={'next'}
+      >
+        &gt;
+      </button>
+      <div className="carousel__index-buttons">
+        <button
+          className="carousel__index-buttons__button"
+          onClick={onClick}
+          value={0}
+        ></button>
+        <button
+          className="carousel__index-buttons__button"
+          onClick={onClick}
+          value={1}
+        ></button>
+        <button
+          className="carousel__index-buttons__button"
+          onClick={onClick}
+          value={2}
+        ></button>
+        <button
+          className="carousel__index-buttons__button"
+          onClick={onClick}
+          value={3}
+        ></button>
+        <button
+          className="carousel__index-buttons__button"
+          onClick={onClick}
+          value={4}
+        ></button>
+      </div>
     </div>
-    )
+  );
 }
--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Header({ onClick, openLogin, onCloseLogin }) {
   const [openDialog, setOpenDialog] = useState(0);
